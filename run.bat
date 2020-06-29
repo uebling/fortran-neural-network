7996ed89d60d497710f091c56113e7d8ebb63634
Ulrich Ebling <uebling97@gmail.com>
2020-06-29 22:59:48 +1200

    less stupid compile scripts

--- run.bat
+++ run.bat
@@ -1,5 +1,3 @@
-gfortran Parameters.f90 -c -fdefault-real-8
-gfortran Nnetwork.f90 -c -fdefault-real-8
-gfortran TrainData.f90 -c -fdefault-real-8
+gfortran Parameters.f90 Nnetwork.f90 TrainData.f90 -c -fdefault-real-8
 gfortran FortranNN.f90 -o a.exe -fdefault-real-8 Parameters.o Nnetwork.o TrainData.o
 a
